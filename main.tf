9742649e4fbf6bc56dfc47f1caa4c4fcd3ddfd81
niha1609 <45825011+niha1609@users.noreply.github.com>
2023-06-13 17:11:11 +0530

    Updated main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,51 @@
+# security group creation and attcahing in ecs, alb etc
+
+# ALB Security Group: Edit to restrict access to the application
+resource "aws_vpc" "default" {
+  cidr_block = "172.31.0.0/16"
+}
+
+resource "aws_security_group" "alb-sg" {
+  name        = "testapp-load-balancer-security-group"
+  description = "controls access to the ALB"
+  vpc_id      = aws_vpc.default.id
+
+  ingress {
+    protocol    = "tcp"
+    from_port   = var.app_port
+    to_port     = var.app_port
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    protocol    = "-1"
+    from_port   = 0
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# this security group for ecs - Traffic to the ECS cluster should only come from the ALB
+resource "aws_security_group" "ecs_sg" {
+  name        = "testapp-ecs-tasks-security-group"
+  description = "allow inbound access from the ALB only"
+  vpc_id      = aws_vpc.default.id
+
+  ingress {
+    protocol        = "tcp"
+    from_port       = var.app_port
+    to_port         = var.app_port
+    security_groups = [aws_security_group.alb-sg.id]
+  }
+
+  egress {
+    protocol    = "-1"
+    from_port   = 0
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 # Define provider for AWS
 provider "aws" {
   region = "us-east-1"  # Update with your desired region
@@ -28,7 +76,7 @@ resource "aws_ecs_task_definition" "my_task_definition" {
   }
 ]
 DEFINITION
-  requires_compatibilities = ["FARGATE"]  # Use "EC2" if you prefer EC2 launch type
+  requires_compatibilities = ["EC2"]  # Use "EC2" if you prefer EC2 launch type
   network_mode             = "awsvpc"
   execution_role_arn       = aws_iam_role.my_task_execution_role.arn
 }
@@ -39,23 +87,23 @@ resource "aws_ecs_service" "my_service" {
   cluster         = aws_ecs_cluster.my_cluster.id
   task_definition = aws_ecs_task_definition.my_task_definition.arn
   desired_count   = 1
-  launch_type     = "FARGATE"  # Use "EC2" if you prefer EC2 launch type
+  launch_type     = "EC2"  # Use "EC2" if you prefer EC2 launch type
 
   network_configuration {
-    subnets         = ["subnet-12345678", "subnet-87654321"]  # Update with your desired subnets
-    security_groups = ["sg-12345678"]  # Update with your desired security groups
-    assign_public_ip = true
+    subnets         = ["subnet-015484ab2a5e58149", "subnet-01d31b21ce5911add"]  # Update with your desired subnets
+    security_groups = ["sg-0797f2482623cc87e"]  # Update with your desired security groups
+    #assign_public_ip = true
   }
 }
 
 # Create an ECR repository
 resource "aws_ecr_repository" "my_repository" {
-  name = "test"  # Update with your desired repository name
+  name = "poc"  # Update with your desired repository name
 }
 
 # Create IAM role for task execution
 resource "aws_iam_role" "my_task_execution_role" {
-  name = "POC"  # Update with your desired role name
+  name = "Test"  # Update with your desired role name
   assume_role_policy = <<POLICY
 {
   "Version": "2012-10-17",
